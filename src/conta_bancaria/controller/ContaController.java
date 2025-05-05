0bfcdb82c8601c509a7a7ad22d49e5ab3ab7bbfa
Pedro Coelho <phcoelho2004@gmail.com>
2025-05-05 11:43:47 -0300

    :sparkles: Implementação dos métodos bancários - Sacar, transferir, depositar

--- src/conta_bancaria/controller/ContaController.java
+++ src/conta_bancaria/controller/ContaController.java
@@ -1,5 +1,6 @@
 package conta_bancaria.controller;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -64,18 +65,48 @@ public class ContaController implements ContaRepository{
 
 	@Override
 	public void sacar(int numero, float valor) {
+		NumberFormat nfMoeda = NumberFormat.getCurrencyInstance();
 		
-		
+		Optional <Conta >conta = buscarNaCollection(numero);
+				
+		if(conta.isPresent()) {
+			if(conta.get().sacar(valor) ==  true)
+				System.out.printf("\nO saque no valor de %s, foi efetuado com sucesso na conta número %d!",nfMoeda.format(valor), numero);
+		}else
+			System.out.printf("\nA conta número %d não foi encontrada!", numero);
+				
 	}
 
 	@Override
 	public void depositar(int numero, float valor) {
+		NumberFormat nfMoeda = NumberFormat.getCurrencyInstance();
 		
+		Optional <Conta >conta = buscarNaCollection(numero);
+		
+		if(conta.isPresent()) {
+			conta.get().depositar(valor);
+			System.out.printf("\nO Depósito no valor de %s, foi efetuado com sucesso na conta número %d!",nfMoeda.format(valor), numero);
+		}else
+			System.out.printf("\nA conta número %d não foi encontrada!", numero);
+				
 		
 	}
 
 	@Override
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		NumberFormat nfMoeda = NumberFormat.getCurrencyInstance();
+		
+		Optional <Conta >contaOrigem = buscarNaCollection(numeroOrigem);
+		Optional <Conta >contaDestino = buscarNaCollection(numeroDestino);
+		
+		if(contaOrigem.isPresent() && contaDestino.isPresent()) {
+			if(contaOrigem.get().sacar(valor) ==  true) {
+				contaDestino.get().depositar(valor);
+				System.out.println("\nA transferência no valor de " + nfMoeda.format(valor) +  " da Conta " + numeroOrigem + " para a conta "+ numeroDestino +" foi efetuada com sucesso! ");
+			}
+		}else
+			System.out.printf("\nA conta número %d não foi encontrada!", numero);
+				
 		
 		
 	}
